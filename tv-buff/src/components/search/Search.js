216ee3f8eb58c3f01ec73a021ef2b4762ac00c9b
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2019-05-13 22:35:44 -0400

    search works, minor bug with onChange, material UI cards added and work, update to come on fetch to make objects

--- tv-buff/src/components/search/Search.js
+++ tv-buff/src/components/search/Search.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import MyBtn from '../button/button'
 
 const Search = props => {
     return(
-        <form>
-            <input placeholder="Search"
+        <form onSubmit={props.search}>
+            <input placeholder="Tom Cruise.."
+            name = 'search'
             style={styles.input} 
-            onChange={props.search}/>
+            onChange={props.search}/>   
+            <MyBtn style={styles.btn} btnText="Search" />
         </form>
     )
 }
@@ -20,4 +23,22 @@ const styles= {
       textIndent: '5%',
       height: '2rem'
     },
+    btn:{
+        MozBoxShadow:'inset 0px 0px 15px 3px #A22C29',
+        WebkitBoxShadow:'inset 0px 0px 15px 3px #A22C29',
+        boxShadow:'inset 0px 0px 15px 3px #A22C29',
+        backgroundColor:'transparent',
+        MozBorderRadius:'17px',
+        WebkitBorderRadius:'17px',
+        borderRadius:'17px',
+        border:'1px solid #1f2f47',
+        display:'inline-block',
+        cursor:'pointer',
+        color:'#ffffff',
+        fontRamily:'Arial',
+        fontRize:'15px',
+        padding:'6px 13px',
+        textDecoration:'none',
+        textShadow:'0px 1px 0px #263666',
+      },
 }  
--- tv-buff/src/pages/Home.js
+++ tv-buff/src/pages/Home.js
@@ -1,41 +1,51 @@
-//import React from 'react'
+
 import React, { Component } from 'react';
 import Search from '../components/search/Search.js';
-import Nav from '../components/nav/Nav.js';
+import Header from '../components/header/Header.js';
+import Card from '../components/card/Card'
 
 class Home extends Component{
   state = {
     err: null,
     isLoaded: false,
-    actors: '',
-    shows: '',
+    //actors and shows need to be made into objects
+    actors: [],
+    shows: [],
     search: '',
   };
 search = e => {
+  e.preventDefault()
   this.setState({search: e.target.value})
-  console.log(this.search)
+  console.log(this.state.search)
+  this.fetchData()
+  //console.log(this.state.search)
 }
 //fetch data twice, once for shows the other for actors
 componentDidMount() {
+  this.fetchData()
+}
+fetchData(){
   Promise.all([
-  fetch("http://api.tvmaze.com/search/people?q=lauren",{method: 'get'}),
-  fetch("http://api.tvmaze.com/search/shows?q=girls",{method: 'get'})
-])
-    .then(([res1, res2])=> Promise.all([res1.json(),res2.json()]))
-    .then(
-      ([data1, data2]) => {this.setState({
-        actors: data1,
-        shows: data2,
-        isLoaded: true,
-      })
-    },
-    (err) => {
-      this.setState({
-        isLoaded: true,
-        err
-      });
-    }
-    );
+    fetch(`http://api.tvmaze.com/search/people?q=:${this.state.search}`,{method: 'get'}),
+    fetch(`http://api.tvmaze.com/search/shows?q=:${this.state.search}`,{method: 'get'})
+  ])
+      .then(([res1, res2])=> Promise.all([res1.json(),res2.json()]))
+      .then(
+        //make actors and shows into objects i.e. ${data1.person}
+        ([data1, data2]) => {this.setState({
+          actors: data1,
+          shows: data2,
+          isLoaded: true,
+        })
+      },
+      (err) => {
+        this.setState({
+          isLoaded: false,
+          err
+        });
+      }
+      );
+
 }
 
 render() {
@@ -48,16 +58,19 @@ render() {
     //console.log(actors)
     return (
       <div>
-        <Nav />
+        <Header />
         <Search search={this.search} />
-        {actors.map(actor => (
-          <li key={actor.person.id}>
-            {actor.person.name} 
-          </li>
-        ))}
+        {actors.map(actor => { const {id, alt, image, title }
+          <Card id={actor.person.id} alt={actor.person.name+" picture"} image={actor.person.image.medium} title={actor.person.name}/>
+          // <li key={actor.person.id}>
+          //   <p>{actor.person.name}</p>
+            
+          // </li>
+        })}
         {shows.map(show => (
           <li key={show.show.id}>
-            {show.show.name} 
+            <p>{show.show.name}</p>
+           
           </li>
         ))}
       </div>
@@ -68,3 +81,5 @@ render() {
 
 export default Home;
 
+ // <img src={show.show.image.medium} />
+            // <a></a>
